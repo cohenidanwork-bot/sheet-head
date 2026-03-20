082148eb8913e505024d6e199c4a3ff47011e2be
Idan Coh <idancoh@users.noreply.github.com>
2026-03-20 02:20:05 +0200

    feat: wire up app entry point with EnvironmentObject and scene phase persistence

--- ShitHead/ShitHeadApp.swift
+++ ShitHead/ShitHeadApp.swift
@@ -1,10 +1,20 @@
+// ShitHead/ShitHeadApp.swift
 import SwiftUI
 
 @main
 struct ShitHeadApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    @StateObject private var vm = GameViewModel()
+
     var body: some Scene {
         WindowGroup {
-            Text("Hello")
+            HomeView()
+                .environmentObject(vm)
+        }
+        .onChange(of: scenePhase) { _, phase in
+            if phase == .background, let state = vm.gameState {
+                GamePersistence.save(state)
+            }
         }
     }
 }
